d13b79974e1daa6f87cb725540823146c2a8ede7
root <root@x86_64.lan>
2018-10-10 10:48:33 -0400

    quick and dirty build-all for 3.10

--- build_all_3_10.sh
+++ build_all_3_10.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+pushd groups/release-3.10/origin-release
+./build.sh
+popd
+
+pushd groups/release-3.10/origin
+./build.sh
+popd
+
+
+pushd groups/release-3.10/image-registry
+./build.sh
+popd
+
+
+pushd groups/release-3.10/origin-web-console-server
+./build.sh
+popd
