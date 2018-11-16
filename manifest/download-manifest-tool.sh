e77f5c463c2ad23594578a34f2874a763db509d7
root <root@sgi-xe500-01.rhts.eng.bos.redhat.com>
2018-11-16 15:01:57 -0500

    remove manifest binary

--- manifest/download-manifest-tool.sh
+++ manifest/download-manifest-tool.sh
@@ -3,6 +3,7 @@
 # From https://github.com/estesp/manifest-tool
 
 #copy one of these
+
 #https://github.com/estesp/manifest-tool/releases/download/v0.8.0/manifest-tool-linux-amd64
 #https://github.com/estesp/manifest-tool/releases/download/v0.8.0/manifest-tool-linux-arm64
 #https://github.com/estesp/manifest-tool/releases/download/v0.8.0/manifest-tool-linux-s390x
